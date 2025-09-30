58dc0384f93f5d6c5879e29d027ec62b539742c5
guilhermeramosalvim <guilherme_alvim@estudante.sesisenai.org.br>
2025-09-30 12:57:13 -0300

    wifi basico

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08"; 
 
+void setup() {
+  Serial.begin(115200); //configura a placa para mostrar na tela
+  WiFi.begin(SSID, PASS); //tenta conectar na rede
+  Serial.println("conectado no WiFi");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("conectado com sucesso");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
