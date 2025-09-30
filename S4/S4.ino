305a424b3543ba2a43f44ca3cf10740b05349f9c
gs67396 <gabriel_santos67@estudante.sesisenai.org.br>
2025-09-30 12:44:11 -0300

    Conexão basica adicionada ao S3

--- S4/S4.ino
+++ S4/S4.ino
@@ -1,6 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+const String SSID = "nome da rede";
+const String PASS = "8120gv08";
 
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(SSID, PASS);
+  Serial.println("conectando no WiFi");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.println("*");
+    delay(200);
+  }
+  Serial.println("conectando com sucesso");
 }
 
 void loop() {
