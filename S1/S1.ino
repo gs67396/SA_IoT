7edaffe6c998d781b07183c0684905a35ffa6b4c
CAIO ALVES DA MAIA <caio_a_maia@estudante.sesisenai.org.br>
2025-10-07 12:28:51 -0300

    S1 atualizado

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,6 +1,17 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;
+PubSubClient mqtt(client);
+
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
+
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+
+const String brokerUser = "";
+const String brokerpass = "";
 
 void setup() {
   Serial.begin(115200);
@@ -11,6 +22,18 @@ void setup() {
     delay(200);
   }
   Serial.println("Conectado com sucesso!");
+
+  mqtt.setServer(brokerURL.c_str(),brokerPort);
+  Serial.println("Conectando no Broker");
+
+  String boardID = "Sl-";
+  boardID += String(random(0xffff),HEX);
+
+  while(!mqtt.connect(boardID.c_str())){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado com sucesso ao broker!");
 }
 
 void loop() {
