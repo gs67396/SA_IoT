580ea30461c3c54eb8a2c296439dae57deac46c2
gs67396 <gabriel_santos67@estudante.sesisenai.org.br>
2025-10-07 12:22:47 -0300

    update no S4

--- S4/S4.ino
+++ S4/S4.ino
@@ -1,6 +1,17 @@
 #include <WiFi.h>
-const String SSID = "nome da rede";
+#include <PubSubClient.h>
+
+WiFiClient client; 
+PubSubClient mqtt(client);
+
+const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
+
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+
+const String brokerUser = "";
+const String brokerPass = "";
 
 void setup() {
   Serial.begin(9600);
@@ -10,7 +21,17 @@ void setup() {
     Serial.println("*");
     delay(200);
   }
-  Serial.println("conectando com sucesso");
+  Serial.println("\nconectando com sucesso");
+
+  String boardID = "S4- ";
+  boardID += String(random(0xffff),HEX);
+
+  mqtt.setServer(brokerURL.c_str(),brokerPort);
+  Serial.println("Conectadno no Broker");
+  while(mqtt.connect(boardID.c_str())){
+    Serial.print("*");
+    delay(200);
+  }
 }
 
 void loop() {
