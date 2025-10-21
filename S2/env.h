7c00ea4ab985e46138d0ed52b3f812b72d3539de
guilhermeramosalvim <guilherme_alvim@estudante.sesisenai.org.br>
2025-10-21 12:26:20 -0300

    a

---
+++ S2/env.h
@@ -0,0 +1,17 @@
+#ifndef ENV_H
+#define ENV_H
+
+// Variáveis do wifi
+#define SSID "FIESC_IOT_EDU"
+#define PASS "8120gv08"
+// variaveis do bloker
+#define BROKER_URL "832e4ca5c2d24526b737be9ef4ea7eb8.s1.eu.hivemq.cloud"
+#define BROKER_PORT 8883
+
+                                                                                     //Daqui para baixo, do grupo as informacoes | | | | | | | | | | | | | | | | | | | | | | | | | | | | | || | | | || | | | |
+// Variaveis do user dpo broker
+#define BROKER_USR_NAME ""
+#define BROKER_USR_PASS ""
+
+// variavies dos topicos
+#define TOPIC1 ""
