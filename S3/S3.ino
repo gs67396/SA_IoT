6f08ede8843f46c012a2f4c914e5b074e349f040
gs67396 <gabriel_santos67@estudante.sesisenai.org.br>
2025-11-19 11:02:58 -0300

    Atualização S3

--- S3/S3.ino
+++ S3/S3.ino
@@ -3,12 +3,18 @@
 
 WiFiClientSecure WiFiClient;
 PubSubClient mqtt(client);
+const byte TRIGGER_PIN = 5;
+const byte ECHO_PIN = 18;
+
 
 void setup() {
   //servo motor 1
   meuServo.attach(9);
   //servo motor 2
   meuServo.attach(13);
+
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 
   //led rgb
   pinMode(redPin, OUTPUT);
@@ -47,10 +53,37 @@ void setup() {
   Serial.println("conectado com sucesso!");
 }
 
+long lerDistancia() {
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  
+  long duracao = pulseIn(ECHO_PIN, HIGH);
+  long distancia = duracao * 349.24 / 2 / 10000;
+  
+  return distancia;
+}
+
+
 void loop() {
 
   // led rgb 1
   //cor vermelha
+  long distancia = lerDistancia();
+
+   Serial.print("Distância: ");
+  Serial.print(distancia);
+  Serial.println(" cm");
+  
+  if (distancia < 10) {
+    Serial.println("Objeto próximo!");
+  }
+  
+  delay(500);
+}
+
 
   analogWrite(redPin, 255);
   analogWrite(greenPin, 0);
